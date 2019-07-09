41c6fb0d7ce210a1b19fe3e894d646b1c18accd1
bastienwcs <email>
2019-07-09 14:30:05 +0200

    add task repository

--- src/main/java/com/example/authentication/controller/UserController.java
+++ src/main/java/com/example/authentication/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.authentication.controller;
 
 import com.example.authentication.entity.User;
+import com.example.authentication.repository.TaskRepository;
 import com.example.authentication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,9 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private TaskRepository taskRepository;
+
     @PostMapping("/signIn")
     public User signIn(@RequestBody User user) {
         return userRepository.findByEmailIgnoreCaseAndPassword(user.getEmail(),
@@ -21,6 +25,15 @@ public class UserController {
 
     @PostMapping("/signUp")
     public User signUp(@RequestBody User user) {
+        // TODO : add apikey
         return userRepository.save(user);
     }
+
+    // TODO : update only if user is allowed
+
+    // TODO : add task only if user is allowed
+
+    // TODO : list tasks only if user is allowed
+
+    // TODO : remove task only if user is allowed
 }
